fc8ef943f9337be291f22c506010e692bc6b3aa5
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-07-16 09:29:08 -0600

    feat(): Calendar

--- constants.js
+++ constants.js
@@ -1,10 +1,9 @@
 export const event = [
   {
-    slug: "taylor-musuc-fest",
+    slug: "taylor-music-fest",
     imagen: "/images/1.jpg",
     nombre: "Taylor Music Fest",
-    fecha: "10 Nov. 2024",
-    hora: "12:00 am",
+    date: "2024-07-20T18:00:00-04:00",
     descripcion:
       "Taylor Music Fest es el evento musical más esperado del año, donde podrás disfrutar de una experiencia única e inolvidable. Este festival contará con la presencia de la icónica Taylor Swift, quien ofrecerá un concierto espectacular con sus mayores éxitos y nuevas canciones. Además, la Banda X se unirá al espectáculo, brindando su energía y talento en el escenario. El festival no solo será un deleite musical, sino que también habrá una gran variedad de puestos de comida, actividades interactivas, y áreas de descanso para que puedas disfrutar al máximo de este día lleno de diversión y entretenimiento. ",
     invitados: "Taylor Swift, Banda X",
@@ -15,60 +14,56 @@ export const event = [
     slug: "dj-fest-vip",
     imagen: "/images/2.jpg",
     nombre: "DJ's Fest VIP",
-    fecha: "15 Nov. 2024",
-    hora: "10:00 am",
+    date: "2024-07-15T20:00:00-07:00",
     descripcion: "Explora las obras más recientes de artistas contemporáneos.",
     invitados: "Artista A, Artista B",
     costo: "50",
-    lugar: "San Francisco",
+    lugar: "San Francisco"
   },
   {
     slug: "taller-nocturno",
     imagen: "/images/3.jpg",
     nombre: "Taller Nocturno",
-    fecha: "20 Nov. 2024",
-    hora: "7:00 pm",
+    date: "2024-07-10T21:00:00-07:00",
     descripcion:
       "Aprende técnicas avanzadas para capturar fotos nocturnas impresionantes.",
     invitados: "Fotógrafo C, Fotógrafo D",
     costo: "120",
-    lugar: "Los Angeles",
+    lugar: "Los Angeles"
   },
   {
     slug: "international-fest",
     imagen: "/images/4.jpg",
     nombre: "Internacional Fest",
-    fecha: "25 Nov. 2024",
-    hora: "11:00 am",
+    date: "2024-10-05T12:00:00-05:00",
     descripcion: "Saborea platillos de diferentes culturas en un solo lugar.",
     invitados: "Chef E, Chef F",
     costo: "30",
-    lugar: "Chicago",
+    lugar: "Chicago"
   },
   {
     slug: "festival-5k",
     imagen: "/images/5.jpg",
     nombre: "Festival 5K",
-    fecha: "30 Nov. 2024",
-    hora: "7:00 am",
+    date: "2024-11-01T08:00:00-04:00",
     descripcion:
       "Únete a nuestra carrera para promover un estilo de vida saludable.",
     invitados: "Atleta G, Atleta H",
     costo: "25",
-    lugar: "Miami",
+    lugar: "Miami"
   },
   {
     slug: "conferencia-innovacion",
     imagen: "/images/6.jpg",
     nombre: "Conferencia de Innovación",
-    fecha: "5 Dic. 2024",
-    hora: "9:00 am",
+    date: "2024-12-18T09:00:00-05:00",
     descripcion: "Descubre las últimas tendencias y avances en tecnología.",
     invitados: "Ponente I, Ponente J",
     costo: "150",
-    lugar: "Boston",
-  },
+    lugar: "Boston"
+  }
 ];
+
 
 export const members = [
     { name: "Emma Smith" },
---
+++ src/pages/date.js
@@ -0,0 +1,27 @@
+import { setHours, setMinutes } from 'date-fns';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default function DateComponent() {
+  const [startDate, setStartDate] = useState(
+    setHours(setMinutes(new Date(), 0), 9)
+  );
+
+  const filterPassedTime = (time) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
+  };
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      showTimeSelect
+      filterTime={filterPassedTime}
+      dateFormat="MMMM d, yyyy h:mm aa"
+    />
+  );
+}
