2ae6b1ba166b26a6d78a5a882d56b4720a93fe32
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2024-07-16 20:38:33 -0600

    feat(): Get by user id/api

--- src/pages/event.js
+++ src/pages/event.js
@@ -9,14 +9,20 @@ import {
   H1Styled,
   Title,
 } from "@/styles/Event.style";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaFire } from "react-icons/fa";
-import { event } from "../../constants";
 import Card from "@/components/Card";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import EventRepo from "@/infraestructure/implementation/httpRequest/axios/EventRepo";
+import GetAllEventUseCase from "@/application/usecases/eventUseCase/GetAllEventUseCase";
 
 export default function Event() {
   const router = useRouter();
+  const userId = useSelector((state) => state.user._id);
+  const [events, setEvents] = useState([]);
+  const eventRepo = new EventRepo();
+  const getAllEventUseCase = new GetAllEventUseCase(eventRepo);
 
   const navigateToEvent = (id) => {
     return router.push({
@@ -28,6 +34,41 @@ export default function Event() {
   const navigateToAdd = () => {
     router.push("/addEvent");
   }
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleDateString("es-ES", { month: "long" });
+    const year = date.getFullYear();
+    return `${day} ${month}, ${year}`;
+  };
+
+  const formatTime = (dateString) => {
+    const date = new Date(dateString);
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const minutesStr = minutes < 10 ? "0" + minutes : minutes;
+    return `${hours}:${minutesStr} ${ampm}`;
+  };
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await getAllEventUseCase.run();
+        const filteredEvents = response.response.events.filter(
+          (event) => event.user._id === userId
+        );
+        setEvents(filteredEvents);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
 
   return (
     <Container>
@@ -48,19 +89,17 @@ export default function Event() {
           <CatButton>Concluidos</CatButton>
         </Categories>
         <EventContainer>
-          {event.map((event, index) => (
-            <Card
-              key={index}
-              name={event.nombre}
-              image={event.imagen}
-              date={event.fecha}
-              time={event.hora}
-              description={event.descripcion}
-              guests={event.invitados}
-              price={event.costo}
-              place={event.lugar}
-              onClick={() => navigateToEvent(event.slug)}
-            />
+          {events.map((event, index) => (
+                <Card
+                key={index}
+                name={event.title}
+                image={event.image.secureUrl}
+                time={formatTime(event.date)}
+                date={formatDate(event.date)}
+                description={event.description}
+                price={event.cost}
+                onClick={() => navigateToEvent(event._id)}
+              />
           ))}
         </EventContainer>
       </EventsContent>
